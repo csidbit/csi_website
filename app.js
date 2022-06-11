51e32515970da5dc9ec3c240e915e005ade4e734
Om-jannu <omjannu2002@gmail.com>
2022-06-11 17:06:36 +0530

    added : navbar

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
-// const tl = gsap.timeline({defalts: {ease: "power2.out"}})
+const tl = gsap.timeline({defalts: {ease: "power2.out"}})
 // tl.to('.hidetext',{y:"0%",duration:1,stagger:0.3});
 // tl.to('.slider',{y:"-100%",duration:1,delay:0.5});
 // tl.to('.landingpage',{y:"-100%",duration:1.5},"-=1");
-// tl.fromTo('.header',{y:"1rem",opacity:0},{y:"0",opacity:1,duration:1});
-// // tl.fromTo('.header',{opacity:0},{opacity:1,duration:1});
-// tl.fromTo('.content',{opacity:0},{opacity:1,duration:1},"-=1.5");
+tl.fromTo('.header',{y:"1rem",opacity:0},{y:"0",opacity:1,duration:1});
+tl.fromTo('.header',{opacity:0},{opacity:1,duration:1});
+tl.fromTo('.content',{opacity:0},{opacity:1,duration:1},"-=1.5");
 
 var header = document.getElementById("header");
 var lastscrolltop = 0;
